142c29e48358b2abdcfe3ce6b1984060d05ce031
Tobi F. <tf4482@gmail.com>
2024-02-28 20:42:00 +0100

    feat: :card_file_box: Added additional proposal fields

--- proposals/models.py
+++ proposals/models.py
@@ -2,5 +2,22 @@ from django.db import models
 
 
 class Proposal(models.Model):
+
+    STATUS_CHOICES = (
+        ("Entered", "Entered"),
+        ("In progress", "n progress"),
+        ("Finished", "Finished"),
+        ("Rejected", "Rejected"),
+    )
+    CATEGORY_CHOICES = (
+        ("General", "General"),
+        ("Classes", "Classes"),
+        ("Adminstrative", "Adminstrative"),
+        ("Technical", "Technical"),
+    )
+
     title = models.CharField(max_length=255)
-    content = models.CharField(max_length=255)
+    content = models.TextField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default="Entered")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default = "General")
+    datetime = models.DateTimeField(auto_now_add=True)
