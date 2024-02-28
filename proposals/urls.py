a9d721a3f2e7f206b0d0a2296823dcd6e4da98ca
Tobi F. <tf4482@gmail.com>
2024-02-29 00:25:52 +0100

    feat: :sparkles: Updated urls

--- proposals/urls.py
+++ proposals/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('proposals/', views.proposals, name='proposals'),
+    path('proposals/proposal_submission', views.proposal_submission, name='proposal_submission'),
     path('proposals/proposal_details/<int:id>', views.proposal_details, name='proposal_details'),
 ]
