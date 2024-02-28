bfab87a37e275a2a6db91eabb1460214ce3a5594
Tobi F. <tf4482@gmail.com>
2024-02-28 22:01:32 +0100

    feat: :sparkles: Added index url and backling from proposal listing

--- proposals/urls.py
+++ proposals/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('proposals/', views.proposals, name='proposals'),
     path('proposals/proposal_details/<int:id>', views.proposal_details, name='proposal_details'),
 ]
