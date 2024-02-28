5848a7edec18fbc0561dd2e40ac05b9a71ab1e18
Tobi F. <tf4482@gmail.com>
2024-02-28 23:30:07 +0100

    refactor: :recycle: Changed view to use render function

--- proposals/views.py
+++ proposals/views.py
@@ -1,23 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.template import loader
 from .models import Proposal
 
 def proposals(request):
-  proposals = Proposal.objects.all().values()
-  template = loader.get_template('all_proposals.html')
-  context = {
-    'proposals': proposals,
-  }
-  return HttpResponse(template.render(context, request))
+    proposals = Proposal.objects.all()
+    return render(request, 'all_proposals.html', {'proposals': proposals})
 
 def proposal_details(request, id):
-  proposal = Proposal.objects.get(id=id)
-  template = loader.get_template('proposal_details.html')
-  context = {
-    'proposal': proposal,
-  }
-  return HttpResponse(template.render(context, request))
+    proposal = Proposal.objects.get(id=id)
+    return render(request, 'proposal_details.html', {'proposal': proposal})
 
 def index(request):
-  template = loader.get_template('index.html')
-  return HttpResponse(template.render())
+    return render(request, 'index.html')
