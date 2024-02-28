aa16f1257b2d4360c2015800f247add30d601625
Tobi F. <tf4482@gmail.com>
2024-02-28 19:39:22 +0100

    feat: :sparkles: Created view for all proposals listing

--- proposals/views.py
+++ proposals/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Proposal
 
 def proposals(request):
-  template = loader.get_template('proposals.html')
-  return HttpResponse(template.render())
+  proposals = Proposal.objects.all().values()
+  template = loader.get_template('all_proposals.html')
+  context = {
+    'proposals': proposals,
+  }
+  return HttpResponse(template.render(context, request))
