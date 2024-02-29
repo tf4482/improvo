4b996173817705462f5a3a219dff08d922ae3b58
Tobi F. <tf4482@gmail.com>
2024-02-29 03:59:09 +0100

    feat: :card_file_box: Added upvotes model and migrations

--- proposals/models.py
+++ proposals/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Proposal(models.Model):
 
     STATUS_CHOICES = (
@@ -22,10 +21,16 @@ class Proposal(models.Model):
     category = models.CharField(
         max_length=20, choices=CATEGORY_CHOICES, default="General"
     )
+    upvotes_count  = models.PositiveIntegerField(default=0)
     datetime = models.DateTimeField(auto_now_add=True)
 
 class Comment(models.Model):
     proposal = models.ForeignKey(Proposal, related_name='comments', on_delete=models.CASCADE)
     author = models.CharField(max_length=255)
     text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Upvote(models.Model):
+    proposal = models.ForeignKey(Proposal, related_name='upvotes', on_delete=models.CASCADE)
+    browser_fingerprint = models.CharField(max_length=32)
     created_at = models.DateTimeField(auto_now_add=True)
