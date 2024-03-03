de714cdb1e9460261ae7b1992027ed141be16715
Tobi F. <tf4482@gmail.com>
2024-03-03 21:17:33 +0100

    style: :art: Reformatted backend code

--- improvo/settings.py
+++ improvo/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+cp#o^7n_qgk0(b$ebrt!l-2d4!%52)u84h$)2(*+gz$6za2vi'
+SECRET_KEY = "django-insecure-+cp#o^7n_qgk0(b$ebrt!l-2d4!%52)u84h$)2(*+gz$6za2vi"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,56 +33,56 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django_bootstrap5',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'proposals'
+    "django_bootstrap5",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "proposals",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'improvo.urls'
+ROOT_URLCONF = "improvo.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.i18n',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.i18n",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'improvo.wsgi.application'
+WSGI_APPLICATION = "improvo.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -92,16 +92,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -109,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = "en"
 
-TIME_ZONE = 'Europe/Berlin'
+TIME_ZONE = "Europe/Berlin"
 
 USE_I18N = True
 
@@ -124,14 +124,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LANGUAGES = [
-    ('de', _('German')),
-    ('en', _('English')),
+    ("de", _("German")),
+    ("en", _("English")),
 ]
--- proposals/models.py
+++ proposals/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.utils.translation import gettext as _
 
+
 class Proposal(models.Model):
 
     STATUS_CHOICES = (
-        (_('Entered'), _('Entered')),
+        (_("Entered"), _("Entered")),
         (_("In progress"), _("In progress")),
         (_("Finished"), _("Finished")),
         (_("Rejected"), _("Rejected")),
@@ -20,21 +21,27 @@ class Proposal(models.Model):
     author = models.CharField(max_length=255, default="Anonymous")
     content = models.TextField()
     status = models.CharField(
-        max_length=20, choices=STATUS_CHOICES, default=_('Entered')
+        max_length=20, choices=STATUS_CHOICES, default=_("Entered")
     )
     category = models.CharField(
         max_length=20, choices=CATEGORY_CHOICES, default=_("General")
     )
-    upvotes_count  = models.PositiveIntegerField(default=0)
+    upvotes_count = models.PositiveIntegerField(default=0)
     datetime = models.DateTimeField(auto_now_add=True)
 
+
 class Comment(models.Model):
-    proposal = models.ForeignKey(Proposal, related_name=_('comments'), on_delete=models.CASCADE)
+    proposal = models.ForeignKey(
+        Proposal, related_name=_("comments"), on_delete=models.CASCADE
+    )
     author = models.CharField(max_length=255)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+
 class Upvote(models.Model):
-    proposal = models.ForeignKey(Proposal, related_name=_('upvotes'), on_delete=models.CASCADE)
+    proposal = models.ForeignKey(
+        Proposal, related_name=_("upvotes"), on_delete=models.CASCADE
+    )
     browser_fingerprint = models.CharField(max_length=32)
     created_at = models.DateTimeField(auto_now_add=True)
