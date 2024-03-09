80ac5e7e7172b42098b5b7bb546e6660615f6ed4
Tobi F. <tf4482@gmail.com>
2024-03-09 15:32:12 +0100

    refactor: :fire: Axed dysfunctional language select feature

--- proposals/urls.py
+++ proposals/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.views.generic.base import TemplateView
 from django.urls import path, include
 from . import views
-from .views import language_select
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,6 +12,5 @@ urlpatterns = [
     path("proposals/proposal_submission", views.proposal_submission, name="proposal_submission"),
     path("proposals/proposal_details/<int:id>", views.proposal_details, name="proposal_details"),
     path("upvote/<int:proposal_id>/", views.upvote_proposal, name="upvote_proposal"),
-    path("select-language/", language_select, name="language_select"),
 ]
 
