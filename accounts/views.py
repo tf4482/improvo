9e065b2f6a55c6e2ad961beff32d4de474ee4bb1
Tobi F. <tf4482@gmail.com>
2024-03-09 14:17:25 +0100

    feat: :sparkles: Added errormessage for existing username

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,20 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+    def form_valid(self, form):
+
+        username = form.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            form.add_error('username', 'This username is already taken.')
+            return self.form_invalid(form)
+        else:
+            return super().form_valid(form)
