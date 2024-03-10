24320d97bbaefd5183352d7cc3d3527c625e880a
Tobi F. <tf4482@gmail.com>
2024-03-10 14:00:53 +0100

    feat: :sparkles: Added reminder for venv deactivation

--- run.bat
+++ run.bat
@@ -3,6 +3,9 @@
 if exist myenv\Scripts\activate (
     echo Virtual environment found. Starting server...
     call myenv\Scripts\activate
+    echo --------------------------------------------------------------------------------------------------------------------------------------
+    echo Keep in mind, after closing the server, you'll have to deactivate the virtual enviroment by yourself by running 'voff' or 'deactivate'
+    echo --------------------------------------------------------------------------------------------------------------------------------------
     python manage.py runserver
 ) else (
     echo Virtual environment not found. Please run the installation script first.
