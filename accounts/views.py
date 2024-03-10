595e58dd890cf00e939c61890161ee24070de660
Tobi F. <tf4482@gmail.com>
2024-03-10 06:19:39 +0100

    refactor: :fire: Removed unused imports

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-from django.views.generic import CreateView
 from django.contrib.auth.models import User
-from django.http import HttpResponseRedirect
 
 
 class SignUpView(generic.CreateView):
